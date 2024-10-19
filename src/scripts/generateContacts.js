0c25f83a5afc8b0b52bbb80d714f4f192707c9e4
Kate <159571202+turchynska@users.noreply.github.com>
2024-10-19 08:07:02 -0400

    finish

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -6,7 +6,7 @@ const generateContacts = async (number) => {
     const contacts = await readContacts();
     const newContact = Array(number).fill(0).map(createFakeContact);
     const result = [...contacts, ...newContact];
-    // await writeContacts(result);
+    await writeContacts(result);
 };
 
 generateContacts(5);
